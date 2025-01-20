a02fce0427c4d919b1ff2def36c6fb359eee9000
Marc Güell Segarra <marc@ondori.dev>
2025-01-20 11:16:57 +0100

    fix: adjust max volume value

--- esphome/components/es8388/es8388_component.cpp
+++ esphome/components/es8388/es8388_component.cpp
@@ -65,8 +65,8 @@ void ES8388Component::setup() {
   this->write_byte(0x04, 0x30);
   this->write_byte(0x03, 0x00);
   // DAC volume max
-  this->write_byte(0x2E, 0x1C);
-  this->write_byte(0x2F, 0x1C);
+  this->write_byte(0x2E, 0x00);
+  this->write_byte(0x2F, 0x00);
   // unmute
   this->write_byte(0x19, 0x00);
 }
